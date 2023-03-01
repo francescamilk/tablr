2cc67709b39959a111bef090b1a060add9bac1b0
Francesca Santoriello <f.santoriello@hotmail.com>
2023-03-01 14:48:52 +0100

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'dishes/index'
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
 
   get '/dinner', to: 'pages#dinner'
   resources :dishes, only: :index
